5ffcca21a543d0939d7911c2816e3b225f313ba6
alvincvms <48074117+alvincvms@users.noreply.github.com>
2020-04-07 18:17:31 -0700

    finished 3-2

--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -23,17 +23,35 @@ class _04_SearchTest {
 	public void testBinarySearch() {
 		//2. use the assertEquals method to test your binary search method.
 		//   remember that the array must be sorted
+		int[] a = {1, 2, 3, 4, 5};
+		int[] b = {10, 20, 30, 40, 50};
+		int[] c = {5, 10, 20};
+		assertEquals(0, _01_BinarySearch.binarySearch(a, 0, 4, 1));
+		assertEquals(0, _01_BinarySearch.binarySearch(b, 0, 4, 10));
+		assertEquals(-1, _01_BinarySearch.binarySearch(c, 0, 1, 20));
 	}
 	
 	@Test
 	public void testInterpolationSearch() {
 		//3. use the assertEquals method to test your interpolation search method.
 		//   remember that the array must be sorted and evenly distributed
+		int[] a = {1, 2, 3, 4, 5};
+		int[] b = {2, 4, 6, 8, 10};
+		int[] c = {3, 6, 9, 12, 15};
+		assertEquals(2, _02_InterpolationSearch.interpolationSearch(a, 3));
+		assertEquals(4, _02_InterpolationSearch.interpolationSearch(b, 10));
+		assertEquals(-1, _02_InterpolationSearch.interpolationSearch(c, 5));
 	}
 	
 	@Test
 	public void testExponentialSearch() {
 		//4. use the assertEquals method to test your exponential search method.
 		//   remember that the array must be sorted
+		int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+		int[] b = {1, 3, 5, 8, 13, 43, 65, 78};
+		int[] c = {2, 4};
+		assertEquals(0, _03_ExponentialSearch.exponentialSearch(a, 1));
+		assertEquals(4, _03_ExponentialSearch.exponentialSearch(b, 13));
+		assertEquals(-1, _03_ExponentialSearch.exponentialSearch(c, 69));
 	}
 }
--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -4,6 +4,91 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-		return 0;
+		for(int i = 0; i < eggs.size(); i++) {
+			if(eggs.get(i).equals("cracked")) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public static int countPearls(List<Boolean> oysters) {
+		int n = 0;
+		for(int i = 0; i < oysters.size(); i++) {
+			if(oysters.get(i)) {
+				n++;
+			}
+		}
+		return n;
+	}
+	
+	public static double findTallest(List<Double> peeps) {
+		double n = peeps.get(0);
+		for(int i = 0; i < peeps.size(); i++) {
+			if(peeps.get(i) > n) {
+				n = peeps.get(i);
+			}
+		}
+		return n;
+	}
+	
+	public static String findLongestWord(List<String> words) {
+		String lw = "";
+		for(int i = 0; i < words.size(); i++) {
+			if(words.get(i).length() > lw.length()) {
+				lw = words.get(i);
+			}
+		}
+		return lw;
+	}
+	
+	public static boolean containsSOS(List<String> message) {
+		for(int i = 0; i < message.size(); i++) {
+			if(message.get(i).equals(" ... --- ... ")) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static List<Double> sortScores(List<Double> results) {
+		boolean sorted = false;
+		while(!sorted) {
+			for(int i = 1; i < results.size(); i++) {
+				if(results.get(i) < results.get(i - 1)) {
+					double a = results.get(i);
+					results.toArray()[i] = results.get(i - 1);
+					results.toArray()[i - 1] = a;
+				}
+			}
+			sorted = true;
+			for(int i = 1; i < results.size(); i++) {
+				if(results.get(i) < results.get(i - 1)) {
+					sorted = false;
+				}
+			}
+		}
+		return results;
+	}
+	
+	public static List<String> sortDNA(List<String> DNA) {
+		List<String> s = DNA;
+		boolean sorted = false;
+		while(!sorted) {
+			for(int i = 1; i < DNA.size(); i++) {
+				if(DNA.get(i).length() < DNA.get(i - 1).length()) {
+					String a = DNA.get(i);
+					DNA.toArray()[i] = DNA.get(i - 1);
+					DNA.toArray()[i - 1] = a;
+				}
+			}
+			sorted = true;
+			for(int i = 1; i < DNA.size(); i++) {
+				if(DNA.get(i).length() < DNA.get(i - 1).length()) {
+					sorted = false;
+				}
+			}
+		}
+		return s;
 	}
 }
